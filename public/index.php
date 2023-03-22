2b7271eab7e175ad388b34db00c65e528cf499f6
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-22 15:32:46 +0100

    connexion3

--- public/index.php
+++ public/index.php
@@ -2,17 +2,16 @@
 
 
 // dépendance :
-require '../config.php';
-require '../model/model.php';
+require_once '../config.php';
+require_once '../model/model.php';
 
 
 // connexion :
 try{
-    $connect = new PDO(DB_TYPE.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET.";",DB_USER, DB_PWD);
+    $connect = new PDO(DB_TYPE.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET.";",DB_USER,DB_PWD);
 }catch(PDOException $e){
     die($e->getMessage());
 }
-
 
 
 // deconnexion :
