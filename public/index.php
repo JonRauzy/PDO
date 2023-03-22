a26d24c0945f74620a6e49190ea4aeb9c2898062
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-22 16:22:52 +0100

    mise en page

--- public/index.php
+++ public/index.php
@@ -1,29 +1,20 @@
 <?php
 
-
 // dépendance :
 require_once '../config.php';
 require_once '../model/model.php';
 
-
 // connexion :
 try{
-
-    $db = new PDO(DB_TYPE.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET.";",DB_USER,DB_PWD);
-    
+    $db = new PDO(DB_TYPE.":host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET.";",DB_USER,DB_PWD);    
     if(ENV==="dev" || ENV === "test"){
-
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     }
-
 }catch(Exception $e){
-
-    die($e->getMessage());
-    
+    die($e->getMessage());    
 }
 
 var_dump(recup($db));
 
 // deconnexion :
-$db = null;
+// $db = null;                       
