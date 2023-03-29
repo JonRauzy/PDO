9d65b017ee31543168227777c3b1de2a6ba907b7
JonRauzy <jonathan.rauzy@gmail.com>
2023-03-29 09:02:09 +0200

    oui

--- view/view.php
+++ view/view.php
@@ -8,42 +8,12 @@
     </head>
     <body>
         <h1>PDO</h1>
-        
-        <h2>pour insérer</h2>
-        <form action="" method="POST">
-            <input type="text" placeholder="title" name="title"><br>
-            <input type="text" name="content" placeholder="content">
-            <input type="submit">
-        </form>
-
-            <!-- <h2>affichage méthode while() et toutes les categories (degolasse) :</h2>
-                <?php 
-                $allCategories = $db->query("SELECT * FROM category ORDER BY title ASC;");
-                while($item = $allCategories->fetch(PDO::FETCH_ASSOC)){
-                    echo "<p>ID: ". $item['id'] . "</p>";
-                }
-                ?>
-                
-
-            <h2>affichage avec findall() et tout les posts : </h2>
-                <?php 
-                if(!empty($allPosts)){
-                    foreach($allPosts as $item){
-                        echo "<p>TITLE : " . $item['title'] . "</p><br>";
-                        echo "<p>Contenu : </p><br><p>" . $item['content'] . "</p><br>";
-                    }
-                }
-                ?> -->
-
-            <h2>execution</h2>
-            <p>oui</p>
-
             <?php 
 
 
             $debut="2023-02-12";
             $fin="2023-03-27 14:30";
-            $sqleu = "SELECT * FROM `post` WHERE `datecreate` < ? AND `datecreate` > ?;"; 
+            $sqleu = "SELECT * FROM `post` ;"; 
             // "?" est un marqueur
             
             $prepare = $db->prepare($sqleu);
@@ -60,6 +30,36 @@
             var_dump($getPostByDate);
 
             ?>
+        
+        <h2>pour insérer</h2>
+        <form action="" method="POST">
+            <input type="text" placeholder="title" name="title"><br>
+            <input type="text" name="content" placeholder="content">
+            <input type="submit">
+        </form>
+
+            <h2>affichage méthode while() et toutes les categories (degolasse) :</h2>
+                <?php 
+                $allCategories = $db->query("SELECT * FROM category ORDER BY title ASC;");
+                while($item = $allCategories->fetch(PDO::FETCH_ASSOC)){
+                    echo "<p>ID: ". $item['id'] . "</p>";
+                }
+                ?>
+                
+
+            <h2>affichage avec findall() et tout les posts : </h2>
+                <?php 
+                if(!empty($allPosts)){
+                    foreach($allPosts as $item){
+                        echo "<p>TITLE : " . $item['title'] . "</p><br>";
+                        echo "<p>Contenu : </p><br><p>" . $item['content'] . "</p><br>";
+                    }
+                }
+                ?>
+
+            <h2>execution</h2>
+            <p>oui</p>
+
 
         
         <!-- var dump frere : -->
